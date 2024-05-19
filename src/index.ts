b689485938aa2499a0c7dc21c9e31b2ddfa99a78
Sergei Dmitriev <iserdmi@gmail.com>
2024-05-19 18:58:24 +0400

    Small fix

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ export type NiceRecord<T> = {
   [K in keyof T]: T[K]
 }
 
-export type Falsy = false | 0 | '' | null | undefined
+export type Falsey = false | 0 | 0n | '' | null | undefined
 
-export type Truthy<T> = T extends Falsy ? never : NonNullable<T>
+export type Truthey<T> = T extends Falsey ? never : T
