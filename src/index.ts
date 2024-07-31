1415379ddb17f7771783c28a3bd68f677a1b9ea0
Sergei Dmitriev <iserdmi@gmail.com>
2024-07-31 17:14:50 +0400

    Small fix

--- src/index.ts
+++ src/index.ts
@@ -11,3 +11,5 @@ export type Maybe<T> = T | Falsey
 export type UnionToTuple<T> = T extends any ? Array<T extends any ? T : never> : never
 
 export type ArrayToTuple<T extends string[]> = T extends Array<infer U> ? U : never
+
+export type CheckIfArraySatisfies<T, U> = [T] extends [U] ? ([U] extends [T] ? true : false) : false
